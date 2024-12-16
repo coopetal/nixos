4bad09a4389b5149bb73e95cb114505141bc4d7c
coopetal <coopetal@proton.me>
2024-12-16 11:06:17 +0000

    Add section for gnome extensions

--- hosts/home-pc/default.nix
+++ hosts/home-pc/default.nix
@@ -227,6 +227,9 @@
       tree
       vim # Do not forget to add an editor to edit configuration.nix! The Nano editor is also installed by default.
       wget
+
+      # Gnome Extensions
+      gnomeExtensions.appindicator
     ];
   };
 
