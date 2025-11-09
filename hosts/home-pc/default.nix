0f9180092a97d9d0e2de01146c547d7d77fb8703
coopetal <coopetal@proton.me>
2025-11-09 23:15:02 +0000

    Add Steam to home-pc. Can remove flathub later

--- hosts/home-pc/default.nix
+++ hosts/home-pc/default.nix
@@ -227,6 +227,16 @@
     zsh.enable = true;
   };
 
+  # Steam settings
+  programs.steam = {
+    enable = true;
+    remotePlay.openFirewall = true;
+    extraCompatPackages = with pkgs; [
+      proton-ge-bin
+    ];
+  };
+  hardware.steam-hardware.enable = true;
+
   # Some programs need SUID wrappers, can be configured further or are
   # started in user sessions.
   # programs.mtr.enable = true;
